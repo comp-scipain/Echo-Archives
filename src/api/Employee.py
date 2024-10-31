7036cea01438be6fc454e90f663bd2e4fd4695f5
comp-scipain <jandroesp1920@gmail.com>
2024-10-30 23:15:57 -0700

    Added the ability to add employees to database

--- src/api/Employee.py
+++ src/api/Employee.py
@@ -28,9 +28,23 @@ def get_employee_stats(Employee: Employee):
     print(f"Pay: {Employee.pay}")
     print(f"Department: {Employee.department}")
 
+
 @router.post("/read")
 def read_employee_stats():
     print("Reading employee data from database")
     with db.engine.begin() as connection:
-        Employee = connection.execute(sqlalchemy.text("SELECT * FROM employees")).all()
-        print(Employee)
+        employee = connection.execute(sqlalchemy.text("SELECT name, skills, pay, department FROM employees")).all()
+        for person in employee:
+            print(f"Employee name: {person.name}")
+            print(f"Employee skills: {person.skills}")
+            print(f"Employee pay: {person.pay}")
+            print(f"Employee Department: {person.department}")
+            
+
+@router.post("/add")
+def add_new_employee(Employee: Employee):
+    print(f"adding employee named {Employee.name} with {Employee.skills} skills being paid ${Employee.pay} to work in {Employee.department}")
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("INSERT INTO employees (name, skills, pay, department) Values (:name, :skills, :pay, :department)"),
+        {"name":Employee.name,"skills": Employee.skills, "pay":Employee.pay,"department":Employee.department})
+        print("Done")
